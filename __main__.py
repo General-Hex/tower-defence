3cb46c71db94a9cf69210131175a2ff0977ecdb5
Ryan Beikrasouli <ryan@Imperial-Console.local>
2025-03-20 11:59:16 +0800

    this is a test this will crash

--- __main__.py
+++ __main__.py
@@ -8,20 +8,20 @@ Futher info: README.md
 
 # IMPORTING MODULES
 
-try: 
-    import pygame
-    pygame.init()
-    pygame.mixer.init()
-except ModuleNotFoundError as err:
-    print(err)
-    print("pygame does not seem to be installed, please install it using: pip install pygame")
-    quit()
+# try: 
+#     import pygame
+#     pygame.init()
+#     pygame.mixer.init()
+# except ModuleNotFoundError as err:
+#     print(err)
+#     print("pygame does not seem to be installed, please install it using: pip install pygame")
+#     quit()
 
-try:
-    from utilities.gameStateManager import GameStateManager
-except ModuleNotFoundError as err:
-    print(err)
-    print("Error missing module please ensure all this games modules are present in their original directory")
+# try:
+#     from utilities.gameStateManager import GameStateManager
+# except ModuleNotFoundError as err:
+#     print(err)
+#     print("Error missing module please ensure all this games modules are present in their original directory")
 
 # Main Game
 if __name__ == '__main__':  

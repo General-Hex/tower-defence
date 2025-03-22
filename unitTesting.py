627ab723d6fbba70aa37edfe977310d11098fbc3
Ryan Beikrasouli <ryan@Imperial-Console.local>
2025-03-22 23:06:09 +0800

    finished deliverable 3 and updated unit testing code

--- unitTesting.py
+++ unitTesting.py
@@ -3,12 +3,24 @@ Secondary module used for unit testing
 """
 
 import pytest
+import pygame
+pygame.init()
+pygame.mixer.init()
 
-def square(a):
-    return a*a
+from otherFunctions import Try_Load
 
-def test_square():
-    assert square(2) == 4
-    assert square(3) == 9
-    assert square(-1) == 1
+def test_sound():
+    sound = Try_Load('laser_shot.wav', 'sound')
+    assert 'pygame.mixer.Sound' in str(type(sound))
+
+def test_music():
+    Try_Load('main_theme.mp3', 'music')
+    pygame.mixer.music.play()
+    assert pygame.mixer.music.get_busy()
+
+def test_image():
+    image = Try_Load("Zombie Assets/zombie 1.png", 'image')
+    assert 'pygame.surface.Surface' in str(type(image))
+
+
 
